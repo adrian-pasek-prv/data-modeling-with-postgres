009b720593d5b9451cac7d1de77f2156b0a32edd
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-05-15 06:49:17 +0000

    Add create_tables.py

---
+++ data-modeling-with-postgres/create_tables.py
@@ -0,0 +1,75 @@
+import psycopg2
+
+def create_database():
+    '''
+    - Creates and connects to sparkifydb
+    - Returns a cursor and connection objects
+    '''
+    
+    # Connect to postgres database and setup a cursor plus autocommit
+    conn = psycopg2.connect(dbname='postgres',
+                            user='admin',
+                            password='admin',
+                            host='127.0.0.1')
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    
+    # Drop sparkifydb if exists and create it anew
+    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    # template0 is a pristine template with no encoding involved
+    # it allows to specify a chosen encoding
+    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    
+    # Close connection to postgres database
+    cur.close()
+    
+    # Open connection to sparkifydb
+    conn = psycopg2.connect(dbname='sparkifydb',
+                            user='admin',
+                            password='admin',
+                            host='127.0.0.1')
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    
+    return conn, cur
+
+
+def drop_tables(cur, conn):
+    """
+    Drops each table using the queries in `drop_table_queries` list from sql_queries.py
+    """
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+        
+def create_tables(cur, conn):
+    """
+    Creates each table using the queries in `create_table_queries` list sql_queries.py. 
+    """
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+        
+def main():
+    """
+    - Drops (if exists) and Creates the sparkifydb 
+    
+    - Establishes connection with the sparkifydb and gets
+    cursor to it.  
+    
+    - Drops all the tables.  
+    
+    - Creates all tables needed. 
+    
+    - Finally, closes the connection. 
+    """
+    cur, conn = create_database()
+    
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
