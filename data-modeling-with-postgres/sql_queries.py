ead1c9e1afc1276fe655a6170cc192ed806a3ff2
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-05-16 13:43:15 +0000

    Update sql_queries.py with further SQL statments

--- data-modeling-with-postgres/sql_queries.py
+++ data-modeling-with-postgres/sql_queries.py
@@ -10,13 +10,13 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays (
-        songplay_id INT PRIMARY KEY,
-        start_time TIMESTAMP NOT NULL,
+        songplay_id SERIAL PRIMARY KEY,
+        start_time TIMESTAMP NOT NULL UNIQUE,
         user_id INT NOT NULL,
-        level VARCHAR NOT NULL,
-        song_id VARCHAR NOT NULL,
-        artist_id INT NOT NULL,
-        session_id INT NOT NULL,
+        level VARCHAR,
+        song_id VARCHAR,
+        artist_id INT,
+        session_id INT,
         location VARCHAR,
         user_agent VARCHAR
     );
@@ -67,9 +67,28 @@ time_table_create = ("""
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+  INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+  VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+  ON CONFLICT (start_time) 
+  DO UPDATE SET
+    user_id = EXCLUDED.user_id,
+    level = EXCLUDED.level,
+    song_id = EXCLUDED.song_id,
+    artist_id = EXCLUDED.artist_id,
+    session_id = EXCLUDED.session_id,
+    location = EXCLUDED.location,
+    user_agent = EXCLUDED.user_agent;
 """)
 
 user_table_insert = ("""
+  INSERT INTO users (user_id, first_name, last_name, gender, level)
+  VALUES (%s, %s, %s, %s, %s)
+  ON CONFLICT (user_id) 
+  DO UPDATE SET
+    first_name = EXCLUDED.first_name,
+    last_name = EXCLUDED.last_name,
+    gender = EXCLUDED.gender,
+    level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
@@ -84,15 +103,41 @@ song_table_insert = ("""
 """)
 
 artist_table_insert = ("""
+  INSERT INTO artists (artist_id, name, location, latitude, longitude)
+  VALUES (%s, %s, %s, %s, %s)
+  ON CONFLICT (artist_id) 
+  DO UPDATE SET
+    name = EXCLUDED.name,
+    location = EXCLUDED.location,
+    latitude = EXCLUDED.latitude,
+    longitude = EXCLUDED.longitude;
 """)
 
 
 time_table_insert = ("""
+  INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+  VALUES (%s, %s, %s, %s, %s, %s, %s)
+  ON CONFLICT (start_time) 
+  DO UPDATE SET
+    hour = EXCLUDED.hour,
+    day = EXCLUDED.day,
+    week = EXCLUDED.week,
+    month = EXCLUDED.month,
+    year = EXCLUDED.year,
+    weekday = EXCLUDED.weekday;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+  SELECT
+    songs.song_id
+    ,artists.artist_id
+  FROM songs
+  JOIN artists using (artist_id)
+  WHERE songs.title = %s
+  AND artists.name = %s
+  AND songs.duration = %s
 """)
 
 # QUERY LISTS
